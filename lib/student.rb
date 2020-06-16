f047b22b1e1d3f4413f99650443755fa07ccc639
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-16 23:20:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,4 +56,55 @@ end
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.students_below_12th_grade
+   sql= <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade < 12
+   SQL
+
+   DB[:conn].execute(sql).map do |row|
+     self.new_from_db(row)
+   end
+ end
+
+ def self.first_X_students_in_grade_10(x)
+   sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade = 10
+     LIMIT ?
+   SQL
+
+   DB[:conn].execute(sql, x).map do |row|
+     self.new_from_db(row)
+   end
+ end
+
+ def self.first_student_in_grade_10
+   sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade = 10
+     LIMIT 1
+   SQL
+
+   DB[:conn].execute(sql).map do |row|
+     return self.new_from_db(row)
+   end
+ end
+
+ def self.all_students_in_grade_X(x)
+   sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade = ?
+   SQL
+
+   DB[:conn].execute(sql, x).map do |row|
+     self.new_from_db(row)
+   end
+ end
+
 end
